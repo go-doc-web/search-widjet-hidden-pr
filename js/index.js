e9ee16776b21915b9126052d6652c3e0d9962d32
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2023-10-02 16:27:19 +0300

    end task

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+const placeholder = 'Search...';
+btn.addEventListener('click', () => {
+  if (!input.classList.contains('input-show')) {
+    input.placeholder = placeholder;
+  } else {
+    input.placeholder = '';
+  }
+  input.classList.toggle('input-show');
+});
